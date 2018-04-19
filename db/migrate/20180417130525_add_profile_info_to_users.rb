e3f194a58690c6eb5bfe07d1972e794c67121f5a
Asad Imtiaz <asad.imtiaz@square63.com>
2018-04-19 15:14:57 +0500

    Rails: Add and Edit user profile picture

---
+++ db/migrate/20180417130525_add_profile_info_to_users.rb
@@ -0,0 +1,9 @@
+class AddProfileInfoToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :date_of_birth, :datetime
+    add_column :users, :gender, :integer
+
+  end
+end
