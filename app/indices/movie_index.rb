ff76f201ff9be5aa003e8b8a1173b9ba97636eaa
Asad Imtiaz <asad.imtiaz@square63.com>
2018-05-08 11:45:40 +0500

    Removed Thinking Sphinx for heroku

--- app/indices/movie_index.rb
+++ app/indices/movie_index.rb
@@ -1,10 +1,10 @@
-ThinkingSphinx::Index.define :movie, with: :active_record, delta: true do
+# ThinkingSphinx::Index.define :movie, with: :active_record, delta: true do
 
-  indexes title, sortable: true
-  indexes description
-  indexes actors.name, as: :actors
-  indexes genre
-  indexes reviews.content, as: :reviews
-  indexes ratings.rating, as: :ratings
+#   indexes title, sortable: true
+#   indexes description
+#   indexes actors.name, as: :actors
+#   indexes genre
+#   indexes reviews.content, as: :reviews
+#   indexes ratings.rating, as: :ratings
 
-end
+# end
